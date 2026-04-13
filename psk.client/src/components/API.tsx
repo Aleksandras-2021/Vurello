f4f5d0448c478f59245294206844e04888b9b61f
Aleksandras Bukelis <aleksbukelis@gmail.com>
2026-04-13 23:55:38 +0300

    Added deployment script to EC2

--- psk.client/src/components/API.tsx
+++ psk.client/src/components/API.tsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-export const API_URL = import.meta.env.VITE_API_URL;
+const isLocalHost = import.meta.env.VITE_IS_LOCAL_HOST === 'true';
+
+export const API_URL = isLocalHost
+    ? import.meta.env.VITE_LOCAL_API_URL
+    : import.meta.env.VITE_API_URL;
 
 export const api = axios.create({
     baseURL: `${API_URL}/api/`,
